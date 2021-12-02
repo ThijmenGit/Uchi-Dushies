08ba5a2e5c1966db189269181b82ee90e7e1640a
ThijmenGit <tvandermeulen@deloitte.nl>
2021-12-02 16:40:23 +0100

    Class API

--- InvestmentGame.py
+++ InvestmentGame.py
@@ -1,28 +1,58 @@
 import requests
 
-def Getstockcode():
 
-query = input("What would you like: ")
 
-url = (f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={query}&apikey=73R6EIWTFXMUJFO0')
-r = requests.get(url)
-data = r.json()
+class APIRequest:
+    def __init__(self):
+        return
 
-#first_key = list(colors)[0]
-#first_val = list(colors.values())[0]
+    def getstockcode(self):
+        query = input("What would you like: ")
 
-for item in range(len(list(data.values())[0])):
-    print(list(data.values())[0][item])
+        url = (f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={query}&apikey=73R6EIWTFXMUJFO0')
+        r = requests.get(url)
+        data = r.json()
 
-stockname = input("Enter the symbol of the Stock you want to buy")
+        #first_key = list(colors)[0]
+        #first_val = list(colors.values())[0]
 
-df=[]
+        for item in range(len(list(data.values())[0])):
+            print(list(data.values())[0][item])
 
-response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stockname}&outputsize=full&apikey=73R6EIWTFXMUJFO0")
-raw_data = response.json()
-df.append(raw_data['Time Series (Daily)']['2021-12-01']
-print(df)
+        stockcode = input("Enter the symbol of the Stock you want to buy: ")
+        return stockcode
 
+    def getcurrentvalue(self):
+        df = []
+        stockname = self.getstockcode()
+
+        response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stockname}&outputsize=full&apikey=73R6EIWTFXMUJFO0")
+        raw_data = response.json()
+        df.append(raw_data['Time Series (Daily)']['2021-12-01'])
+        return(df)
+
+    def getspecificvalue(self, stockdate):
+        df = []
+        stockname = self.getstockcode()
+        response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stockname}&outputsize=full&apikey=73R6EIWTFXMUJFO0")
+        raw_data = response.json()
+        df.append(raw_data['Time Series (Daily)'][str(stockdate)])
+        return(df)
+
+
+if __name__=="__main__":
+    API = APIRequest()
+    df = API.getspecificvalue('2020-01-01')
+    print(df)
+
+
+
+
+
+#    car = Car()
+#    car.drive(10)
+#    car.drive(350)
+#    print(car)
 
 # Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
 # See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
