56ff579a809a6366f5ef7bae9dbadfbea5643b9e
ThijmenGit <tvandermeulen@deloitte.nl>
2021-12-03 09:43:12 +0100

    Integrated API + Stockpurchase

--- StockPurchase.py
+++ StockPurchase.py
@@ -1,16 +1,56 @@
+import requests
+
+class APIRequest:
+    def __init__(self):
+        return
+
+    def getstockcode(self):
+        query = input("What would you like: ")
+
+        url = (f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={query}&apikey=73R6EIWTFXMUJFO0')
+        r = requests.get(url)
+        data = r.json()
+
+
+        for item in range(len(list(data.values())[0])):
+            print(list(data.values())[0][item])
+            #make data more legible
+
+        stockcode = input("Enter the symbol of the Stock you want to buy: ")
+        return stockcode
+
+    def getcurrentvalue(self):
+        df = []
+        stockname = self.getstockcode()
+
+        response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stockname}&outputsize=full&apikey=73R6EIWTFXMUJFO0")
+        raw_data = response.json()
+        df.append(raw_data['Time Series (Daily)']['2021-12-01'])
+        return(df)
+        #current date + 5 min series
+        #return symbol, name, most recent value
+
+    def getspecificvalue(self, stockdate):
+        df = []
+        stockname = self.getstockcode()
+        response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stockname}&outputsize=full&apikey=73R6EIWTFXMUJFO0")
+        raw_data = response.json()
+        df.append(raw_data['Time Series (Daily)'][str(stockdate)])
+        return(df)
+        #get date value working
+
+
+
 class StockPurchase:
     def __init__(self):
         self.stock_data = {"Apple": 100, "Google": 50}
         self.stock_to_buy = None
         self.stock_price = None
 
-    def stock_name(self):
-        while True:
-            stock_name = input("Which stock are you interested in to buy? ")
-            if stock_name in self.stock_data:
-                break
-            else:
-                print("This is not an available stock. Please input a valid stock name!")
+
+        #Call Stock API
+        APIRequest.getcurrentvalue()
+
         self.stock_to_buy = stock_name
         self.stock_price = self.stock_data[stock_name]
         print(f"The price of {stock_name} is €{self.stock_price}  per stock")
@@ -25,6 +65,7 @@ class StockPurchase:
             print("Please insert 'y' or 'n'")
             self.agreed_price()
         self.agreed_amount()
+        #feedback to API
 
     def agreed_amount(self):
         try:
