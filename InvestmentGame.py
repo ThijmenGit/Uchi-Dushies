43ed1d2f8254b5e6315da1e2e3599a9abdebe41b
ThijmenGit <tvandermeulen@deloitte.nl>
2021-12-06 10:30:39 +0100

    Latest version before merge

--- InvestmentGame.py
+++ InvestmentGame.py
@@ -79,16 +79,3 @@ if __name__=="__main__":
     stockpackage = API.stockpackage(stockmetadata, stockprice)
     print(stockpackage)
 
-# Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
-# See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-#    if response.status_code != 200:
-#        raise ValueError("Could not retrieve data, code:", response.status_code)
-
-# The service sends JSON data, we parse that into a Python datastructure
-    
-#stockname = KPN
-#currentprice = 11
-#high = x
-#low = y
-
-
