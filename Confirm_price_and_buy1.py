9c246de1d38351438b737f3097513e1044a4ff0c
ThijmenGit <tvandermeulen@deloitte.nl>
2021-12-06 14:04:26 +0100

    API Agnostic

--- Confirm_price_and_buy1.py
+++ Confirm_price_and_buy1.py
@@ -8,7 +8,7 @@ def agree_price(stock_to_buy):
     agreed_price = ''
     while agreed_price.lower() != "y" and agreed_price.lower() != "n":
         print("Please insert 'y' or 'n'")
-        agreed_price = input(f"Do you wish to buy {stock_to_buy['Name']} at {stock_to_buy['Currency']} {stock_to_buy['Buyingprice']}? (y/n) ")
+        agreed_price = input(f"Do you wish to buy {stock_to_buy['Name']} at {stock_to_buy['Currency']} {stock_to_buy['Price']}? (y/n) ")
 
     if agreed_price.lower() == 'y':
         return True
@@ -25,8 +25,8 @@ def buy_stock(stock_to_buy):
             stock_to_buy["Timestamp"] = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
             stock_to_buy["Buy/Sell"] = 1
             portfolio.append(stock_to_buy)
-        print(float(stock_to_buy['Buyingprice']))
-        transaction_price = round(float(stock_to_buy['Buyingprice']) * amount,3)
+        print(float(stock_to_buy['Price']))
+        transaction_price = round(float(stock_to_buy['Price']) * amount,3)
         print(f"The total value of this transaction will be €{transaction_price} ")
         return portfolio
     except ValueError:
@@ -41,16 +41,16 @@ def purchase():
         portfolio = []
         stockmetadata = APIRequest.getstockmetadata()
         stockprice = APIRequest.getcurrentvalue(stockmetadata)
-        stock_to_buy = APIRequest.stockpackage(stockmetadata, stockprice)
+        stock_info = APIRequest.stockpackage(stockmetadata, stockprice)
 
-        outcome = agree_price(stock_to_buy)
+        outcome = agree_price(stock_info)
         if outcome == True:
-            portfolio = buy_stock(stock_to_buy)
-            df = pd.DataFrame(portfolio, columns = ['Tracker', 'Name', 'Region', 'Currency', 'Buyingprice', 'Timestamp', 'Buy/Sell'])
+            portfolio = buy_stock(stock_info)
+            df = pd.DataFrame(portfolio, columns = ['Tracker', 'Name', 'Region', 'Currency', 'Price', 'Timestamp', 'Buy/Sell'])
             print("Current portfolio holds: \n", df, "\n") ## Insert dataframe here?
-            df['Buyingprice'] = df['Buyingprice'].astype(float)
-            portfoliosum = round(df['Buyingprice'].sum(),3)
-            print(f"The value of this transaction is {stock_to_buy['Currency']} {portfoliosum}")
+            df['Price'] = df['Price'].astype(float)
+            portfoliosum = round(df['Price'].sum(),3)
+            print(f"The value of this transaction is {stock_info['Currency']} {portfoliosum}")
             return df
 
         elif outcome == False:
